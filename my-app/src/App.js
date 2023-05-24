3828f10c962a976fbdfed695c6fe11e9d6b99bde
TejasNayak42 <tejasnayak.c42@gmail.com>
2023-05-24 20:01:12 +0530

    used bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-let name="tejas";
+
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact me</li>
-    </nav>
-    <h1 className="name">Hello {name}</h1>
-    <div className="content">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error saepe eveniet labore? Eius, facere esse reprehenderit a perspiciatis rem, harum veritatis non asperiores dolore, nisi facilis atque maiores! Eveniet laboriosam ex qui maiores sequi odit quis unde molestias tempore ipsam!
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="/">Text Utils</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">About</a>
+      </li>
+      
+     
+    </ul>
+    <form className="d-flex  ">
+      <input className="form-control mx-auto" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success  " type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     </>
   );
 }
