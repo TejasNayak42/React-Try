163671431639bfd434469226aab8c97928214289
TejasNayak42 <tejasnayak.c42@gmail.com>
2023-05-27 20:31:19 +0530

    Dark mode on about section complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Form from './components/Form';
+// import Form from './components/Form';
 import About from './components/About';
 
 function App() {
